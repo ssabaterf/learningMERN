ca684fd3ebeddc7e0f718d2df4d9820a95694263
Samuel <ssabaterf@gmail.com>
2020-09-04 02:07:18 -0400

    Installed nodemon

--- api/school/controller/estudianteController.js
+++ api/school/controller/estudianteController.js
@@ -1,6 +1,6 @@
 const estudianteModel = require('../model/estudianteModel');
 
-var createFunction = async function (req, res) {
+var createFunction = async function(req, res) {
     try {
         estudianteNew = {
             nombre: req.body.nombre,
@@ -11,57 +11,52 @@ var createFunction = async function (req, res) {
 
         var data = new estudianteModel(estudianteNew);
         await data.save();
-        res.status(200).json({Status: 'OK', mssg: 'Insertado correctamente'});
-    }
-    catch (e) {
-        res.status(401).json({Status: 'Failed', error: e});
+        res.status(200).json({ Status: 'OK', mssg: 'Insertado correctamente' });
+    } catch (e) {
+        res.status(401).json({ Status: 'Failed', error: e });
     }
 };
 
-var listFunction = async function (req, res) {
+var listFunction = async function(req, res) {
     try {
         var list = await estudianteModel.find({}).lean();
-        res.status(200).json({Status: 'OK', lista: list});
-    }
-    catch (e) {
-        res.status(401).json({Status: 'Failed', error: e});
-    }
-};
-
-var ListOneFunction = async function (req, res) {
-    try {
-        var list = await estudianteModel.findOne({_id:req.params.id}).lean();
-        res.status(200).json({Status: 'OK', lista: list});
-    }
-    catch (e) {
-        res.status(401).json({Status: 'Failed', error: e});
+        res.status(200).json({ Status: 'OK', lista: list });
+    } catch (e) {
+        res.status(401).json({ Status: 'Failed', error: e });
     }
 };
 
-var updateFunction = async function (req, res) {
+var ListOneFunction = async function(req, res) {
     try {
-        var estudiante = await estudianteModel.findOne({_id:req.params.id});
+        var list = await estudianteModel.findOne({ _id: req.params.id }).lean();
+        res.status(200).json({ Status: 'OK', lista: list });
+    } catch (e) {
+        res.status(401).json({ Status: 'Failed', error: e });
+    }
+};
+
+var updateFunction = async function(req, res) {
+    try {
+        var estudiante = await estudianteModel.findOne({ _id: req.params.id });
         estudiante.set({
-            nombre: req.body.nombre? req.body.nombre:estudiante.nombre,
-            edad: req.body.edad? req.body.edad:estudiante.edad,
-            notas: req.body.notas? req.body.notas:estudiante.notas,
+            nombre: req.body.nombre ? req.body.nombre : estudiante.nombre,
+            edad: req.body.edad ? req.body.edad : estudiante.edad,
+            notas: req.body.notas ? req.body.notas : estudiante.notas,
         });
         await estudiante.save();
-        res.status(200).json({Status: 'OK', Est: estudiante});
-    }
-    catch (e) {
-        res.status(401).json({Status: 'Failed', error: e});
+        res.status(200).json({ Status: 'OK', Est: estudiante });
+    } catch (e) {
+        res.status(401).json({ Status: 'Failed', error: e });
     }
 };
 
-var deleteFunction = async function (req, res) {
+var deleteFunction = async function(req, res) {
     try {
-        var list = await estudianteModel.findOne({_id:req.params.id});
+        var list = await estudianteModel.findOne({ _id: req.params.id });
         var stats = await list.remove();
-        res.status(200).json({Status: 'OK', Stat: stats});
-    }
-    catch (e) {
-        res.status(401).json({Status: 'Failed', error: e});
+        res.status(200).json({ Status: 'OK', Stat: stats });
+    } catch (e) {
+        res.status(401).json({ Status: 'Failed', error: e });
     }
 };
 
